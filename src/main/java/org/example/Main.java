d9880d5f9bfa0868a6b71ee696a3a3985a2bacf6
student <student@itpark>
2022-06-04 16:33:01 +0300

    http server started

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,20 +2,51 @@ package org.example;
 
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
     public static void main(String[] args) {
         try (
                 ServerSocket serverSocket = new ServerSocket(9999)
-        ){
+        ) {
             while (true) {
-                Socket socket = serverSocket.accept();
-                System.out.println("accepted");
-            }
-        }
-        catch (IOException e) {
+                try (
+                        final Socket socket = serverSocket.accept();
+                        final OutputStream out = socket.getOutputStream();
+                        final InputStream in = socket.getInputStream();
+                ) {
+                    System.out.println(socket.getInetAddress());
+                    out.write("Enter command\n".getBytes(StandardCharsets.UTF_8));
+
+                    //внутренний цикл
+                    final byte[] buffer = new byte[4096];
+                    int offset = 0;
+                    int length = buffer.length;
+                    while (true) {
+                       final int read = in.read(buffer, offset, length);
+                       offset+=read;
+                       length=buffer.length- offset;
+                       final byte lastByte = buffer[offset -1];
+                       if (lastByte == '\n'){
+//                        final int read = in.read(buffer); // read- сколько байт было прочитано
+//                        String message = new String(buffer, 0, read, StandardCharsets.UTF_8);
+//                        System.out.println("message = " + message);
+//                        if (message.endsWith("\n")) {
+                            break;
+                        }
+                        }
+                    final String message = new String(buffer, 0, buffer.length - length, StandardCharsets.UTF_8).trim();
+                    System.out.println("message = " + message);
+                } catch(Exception e){ //если произошла проблема с клиентом
+                        e.printStackTrace();
+                    }
+                }
+        } catch (IOException e) {  //если не удалось запустить сервер
+            e.printStackTrace();
         }
     }
 }
