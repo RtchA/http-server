79959a9a2c14ddd8d5f2a3a19f4e2ac1ff3f27ea
student <student@itpark>
2022-06-04 17:20:46 +0300

    Exctacted Methods

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,33 +14,9 @@ public class Main {
                 ServerSocket serverSocket = new ServerSocket(9999)
         ) {
             while (true) {
-                try (
-                        final Socket socket = serverSocket.accept();
-                        final OutputStream out = socket.getOutputStream();
-                        final InputStream in = socket.getInputStream();
-                ) {
-                    System.out.println(socket.getInetAddress());
-                    out.write("Enter command\n".getBytes(StandardCharsets.UTF_8));
-
-                    //внутренний цикл
-                    final byte[] buffer = new byte[4096];
-                    int offset = 0;
-                    int length = buffer.length;
-                    while (true) {
-                       final int read = in.read(buffer, offset, length);
-                       offset+=read;
-                       length=buffer.length- offset;
-                       final byte lastByte = buffer[offset -1];
-                       if (lastByte == '\n'){
-//                        final int read = in.read(buffer); // read- сколько байт было прочитано
-//                        String message = new String(buffer, 0, read, StandardCharsets.UTF_8);
-//                        System.out.println("message = " + message);
-//                        if (message.endsWith("\n")) {
-                            break;
-                        }
-                        }
-                    final String message = new String(buffer, 0, buffer.length - length, StandardCharsets.UTF_8).trim();
-                    System.out.println("message = " + message);
+                try {
+                final Socket socket = serverSocket.accept();
+                    handleClient(socket);
                 } catch(Exception e){ //если произошла проблема с клиентом
                         e.printStackTrace();
                     }
@@ -48,5 +24,40 @@ public class Main {
         } catch (IOException e) {  //если не удалось запустить сервер
             e.printStackTrace();
         }
+    }
+
+    private static void handleClient(Socket socket) throws IOException {
+        try (
+                final OutputStream out = socket.getOutputStream();
+                final InputStream in = socket.getInputStream();
+        ) {
+            System.out.println(socket.getInetAddress());
+            out.write("Enter command\n".getBytes(StandardCharsets.UTF_8));
+
+            //внутренний цикл
+            final String message = readMessage(in);
+            System.out.println("message = " + message);
+        }
+    }
+
+    private static String readMessage(final InputStream in) throws IOException {
+        final byte[] buffer = new byte[4096];
+        int offset = 0;
+        int length = buffer.length;
+        while (true) {
+           final int read = in.read(buffer, offset, length);
+           offset+=read;
+           length=buffer.length- offset;
+           final byte lastByte = buffer[offset -1];
+           if (lastByte == '\n'){
+//                        final int read = in.read(buffer); // read- сколько байт было прочитано
+//                        String message = new String(buffer, 0, read, StandardCharsets.UTF_8);
+//                        System.out.println("message = " + message);
+//                        if (message.endsWith("\n")) {
+                break;
+            }
+            }
+        final String message = new String(buffer, 0, buffer.length - length, StandardCharsets.UTF_8).trim();
+        return message;
     }
 }
